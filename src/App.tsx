56aee0065e6f485032b395fba833727ada57dddd
Govind Kumar <govindsha7630@gmail.com>
2026-03-22 10:28:12 +0530

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+
+function App() {
+  return(
+  <>
+    <div className="text-white text-6xl">govind Kumar</div>
+  </>)
+}
+
+export default App;
